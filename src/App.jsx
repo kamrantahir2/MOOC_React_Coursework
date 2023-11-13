dfcdcfb51a0af44bf1ff30164827448fabaa1a2b
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-11-13 14:45:25 +0000

    Add buttons for left and right as well as button pressed history

--- src/App.jsx
+++ src/App.jsx
@@ -35,13 +35,40 @@ const Display = ({ counter }) => {
   return <div>{counter}</div>;
 };
 
-function App() {
+const History = ({ allClicks }) => {
+  if (allClicks.length === 0) {
+    return <div>The app is used by pressing the buttons</div>;
+  }
+  return <div>Button press history: {allClicks.join(" ")}</div>;
+};
+
+const App = () => {
   const [counter, setCounter] = useState(0);
   console.log("rendering with counter value", counter);
 
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  };
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    // We create a variable below to avoid the differences with asynchrounisity
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
+  };
+
   const incerement = () => {
     console.log("increasing, value before", counter);
-    setCounter((prev) => (prev += 1));
+    setCounter(counter + 1);
   };
 
   const reset = () => {
@@ -89,8 +116,16 @@ function App() {
         <Button onClick={decrement} text="Decrement" />
         <Button onClick={reset} text="Reset" />
       </div>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text="Left" />
+        <Button onClick={handleRightClick} text="Right" />
+        {right}
+        <History allClicks={allClicks} />
+        <p>Total: {total}</p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;

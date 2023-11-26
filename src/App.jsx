99be40ede4f4a65ddb064784b429a7d672ec7e84
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-11-26 17:39:44 +0000

    Create method to change note's important status

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,16 @@ const App = () => {
     ? notes
     : notes.filter((note) => note.important === true);
 
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((n) => (n.id !== id ? n : response.data)));
+    });
+  };
+
   useEffect(() => {
     console.log("effect");
     axios.get("http://localhost:3001/notes").then((response) => {
@@ -48,7 +58,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
 

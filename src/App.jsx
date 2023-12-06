b2267f28eb7694d86cfcfaf17a5e6861f6981523
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-12-06 14:37:24 +0000

    Add Error state to App component, add Notification component to App component to display error message, add styling to error message

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { useState, useEffect } from "react";
 import Note from "./components/Note";
 import axios from "axios";
 import noteService from "./services/notes";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("some error happened...");
 
   const addNote = (event) => {
     event.preventDefault();
@@ -41,7 +43,12 @@ const App = () => {
         setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)))
       )
       .catch((error) => {
-        alert(`the note ${note.content} has already been deleted`);
+        setErrorMessage(
+          `Note ${note.content} was already removed from the server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter((n) => n.id !== id));
       });
   };
@@ -57,6 +64,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}

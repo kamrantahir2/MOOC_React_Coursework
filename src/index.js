4a428ab5def1ce12f58b7213752be90478b4fd9e
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-12-13 19:44:38 +0000

    Create /api/notes endpoint which returns saved notes

--- src/index.js
+++ src/index.js
@@ -25,6 +25,10 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello World</h1>");
 });
 
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);

485e066eac2514198f7dca8817e486d216bcbc93
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-12-20 20:10:39 +0000

    Create mongo.js file, create collection in this file then save some notes to db

--- mongo.js
+++ mongo.js
@@ -3,3 +3,25 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+const url = `mongodb+srv://kamrantahir117:${process.env.PASSWORD}@cluster0.qkxhsol.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = {
+  content: String,
+  important: Boolean,
+};
+
+const Note = mongoose.model("note", noteSchema);
+
+const note = new Note({
+  content: "Mongoose makes things easy",
+  important: true,
+});
+
+note.save().then((result) => {
+  console.log("Note saved");
+  mongoose.connection.close();
+});

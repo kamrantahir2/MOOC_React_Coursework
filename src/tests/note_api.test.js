83cfa9ec78c045921b268e05b9c6448b18565be4
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-01-04 20:27:35 +0000

    Refactor beforeEach function

--- src/tests/note_api.test.js
+++ src/tests/note_api.test.js
@@ -8,10 +8,9 @@ import helper from "./test_helper.js";
 
 beforeEach(async () => {
   await Note.deleteMany({});
-  let noteObject = new Note(helper.initialNotes[0]);
-  await noteObject.save();
-  noteObject = new Note(helper.initialNotes[1]);
-  await noteObject.save();
+  const noteObjects = helper.initialNotes.map((note) => new Note(note));
+  const promiseArray = noteObjects.map((note) => note.save());
+  await Promise.all(promiseArray);
 });
 
 test("notes are returned as json", async () => {

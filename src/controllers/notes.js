7a52ad4d0bf936d4e06df8ab16e5be4426cbff99
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-01-04 11:47:29 +0000

    Refactor delete function to use async/await

--- src/controllers/notes.js
+++ src/controllers/notes.js
@@ -40,12 +40,13 @@ notesRouter.post("/", async (request, response, next) => {
   }
 });
 
-notesRouter.delete("/:id", (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
+notesRouter.delete("/:id", async (request, response, next) => {
+  try {
+    await Note.findByIdAndDelete(request.params.id);
+    response.status(204).end();
+  } catch (err) {
+    next(err);
+  }
 });
 
 notesRouter.put("/:id", (request, response, next) => {

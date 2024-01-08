b8ccecde248e1571e57a6916502e07fffb95bfd4
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-01-08 19:35:20 +0000

    Update notes.js to verify if the user exists and apply a token if the user is found

--- src/controllers/notes.js
+++ src/controllers/notes.js
@@ -3,6 +3,9 @@ import Note from "../models/note.js";
 const app = express();
 const notesRouter = express.Router();
 import User from "../models/user.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 notesRouter.get("/", async (request, response) => {
   const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
@@ -18,10 +21,26 @@ notesRouter.get("/:id", async (request, response) => {
   }
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
+
 notesRouter.post("/", async (request, response) => {
   const body = request.body;
 
-  const user = await User.findById(body.userId);
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+
+  if (!decodedToken.id) {
+    return response.status(401).json({
+      error: "token invalid",
+    });
+  }
+
+  const user = await User.findById(decodedToken.id);
 
   const note = new Note({
     content: body.content,

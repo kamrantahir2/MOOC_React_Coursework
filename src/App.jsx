6e92d5b38973d930d5986007df4ba484e5e517b0
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-01-10 12:25:09 +0000

    Create login component for frontend

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,21 @@ const App = () => {
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+      console.log(initialNotes);
+    });
+  }, []);
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log("logging in with", username, password);
+  };
 
   const addNote = (event) => {
     event.preventDefault();
@@ -59,18 +74,31 @@ const App = () => {
     console.log(notes);
   };
 
-  useEffect(() => {
-    console.log("effect");
-    noteService.getAll().then((initialNotes) => {
-      setNotes(initialNotes);
-      console.log(initialNotes);
-    });
-  }, []);
-
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}

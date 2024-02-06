1329432a5cb8ebc6b0b546d3f24b4bd9a3638fe9
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-02-06 19:58:08 +0000

    Create test for NEW_NOTE action and create TOGGLE_IMPORTANCE action

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,8 +1,18 @@
 const noteReducer = (state = [], action) => {
-  if (action.type === "NEW_NOTE") {
-    return state.concat(action.payload);
+  switch (action.type) {
+    case "NEW_NOTE":
+      return state.concat(action.payload);
+    case "TOGGLE_IMPORTANCE":
+      const id = action.payload.id;
+      const noteChange = state.find((n) => n.id === id);
+      const changedNote = {
+        ...noteChange,
+        important: !noteChange.important,
+      };
+      return state.map((note) => (note.id !== id ? note : changedNote));
+    default:
+      return state;
   }
-  return state;
 };
 
 export default noteReducer;

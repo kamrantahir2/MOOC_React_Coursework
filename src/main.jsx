19736b064d1246e20befaed6c8a9e473af6a9e2b
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-03-08 18:00:23 +0000

    Revert "Create test for TOGGLE_IMPORTANCE action"
    This reverts commit 9a7c03f2c056e90f50bb078ccedc1dddf5271be2.

--- src/main.jsx
+++ src/main.jsx
@@ -4,23 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { useState } from "react";
 import axios from "axios";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-// const notes = [
-//   {
-//     id: 1,
-//     content: "HTML is easy",
-//     important: true,
-//   },
-//   {
-//     id: 2,
-//     content: "Browser can execute only JavaScript",
-//     important: false,
-//   },
-//   {
-//     id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     important: true,
-//   },
-// ];
+import noteReducer from "./reducers/noteReducer.js";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const store = createStore(noteReducer);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
